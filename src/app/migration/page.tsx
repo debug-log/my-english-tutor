ca0f50da4c731023be52d8cad52cb30e53dc2fa6
debug-log <achileus2003@naver.com>
2025-12-27 21:02:14 +0900

    chore: remove migration scripts and components

--- src/app/migration/page.tsx
+++
@@ -1,9 +0,0 @@
-import SupabaseMigration from "@/components/SupabaseMigration";
-
-export default function MigrationPage() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-gray-50">
-            <SupabaseMigration />
-        </div>
-    );
-}
